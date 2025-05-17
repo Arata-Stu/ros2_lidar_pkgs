90c444e880bfd9bff8099fbb72e980b8543ae72b
Arata Tanaka <ky.37f.9850@s.thers.ac.jp>
2025-05-17 09:56:56 +0900

    add yaml

--- gnn_node/setup.py
+++ gnn_node/setup.py
@@ -1,14 +1,16 @@
 from setuptools import setup, find_packages
+import os
 
 package_name = 'gnn_node'
 
 setup(
     name=package_name,
     version='0.1.0',
-    packages=find_packages(),  # ここでgnn_node配下のすべてのパッケージを自動検出
+    packages=find_packages(),  # 全てのパッケージを自動検出
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'config'), ['config/model.param.yaml'])
     ],
     install_requires=[
         'setuptools',
